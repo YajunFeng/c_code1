f1312855f1979488b80c848cae5cbe7543f94953
YajunFeng <1828212274@qq.com>
2021-10-26 22:00:28 +0800

    测试代码

---
+++ test_08_06/test_08_06/test_01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+	int main()
+{
+	//printf("%d\n",sizeof(char));
+    short age = 20;
+	char ch = 'k';
+	float weight = 95.6f;
+	printf("%c\n",ch);
+	
+	return 0;
+}
---
+++ test_08_06/test_08_06/test_02.c
@@ -0,0 +1,14 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+enum Color
+{
+   YELLOW,
+   BLUE,
+   RED
+};
+int main()
+{ 
+    //enum Color color=BLUE;
+	printf("%d\n",BLUE);
+	return 0;
+}
